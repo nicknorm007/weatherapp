cd086335d916ca37dfe92c5a6173d38b5d51f268
Nick Norman <nicknorm007@gmail.com>
2020-12-27 11:04:06 -0500

    add data

--- controllers/weatherController.js
+++ controllers/weatherController.js
@@ -4,9 +4,9 @@ const service = require('../services/weatherService')
 const weatherController = {
     getWeather (req, res) {
         const data = service.getWeather().then( (result) => {
-          const forecasts = result.data.properties.periods  
-          console.log(forecasts[0].shortForecast);
-          res.render('weather', { title: 'Weather data' });
+          const forecasts = result.data.properties.periods
+          console.log(forecasts[0])  
+          res.render('weather', { title: 'Weather data', forecasts: forecasts});
         });
     }
 }
--- services/weatherService.js
+++ services/weatherService.js
@@ -3,10 +3,17 @@ const Constants = require('../Constants')
 
 const weatherService = {
 
-    async getWeather () {
-        const response = await axios.get(Constants.local_01876_forecast_url)
-        return response
-      }
+  async getWeather() {
+
+    try {
+      const response = await axios.get(Constants.local_01876_forecast_url)
+      return response
+    } catch (error) {
+      console.error(error)
+      process.exit(1)
+    }
+
+  }
 }
 
 module.exports = weatherService;
