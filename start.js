f26304e830221763b0aa2d1791cc144b8ea226a4
Nick Norman <nicknorm007@gmail.com>
2021-01-19 23:12:11 -0500

    Add docker file

--- start.js
+++ start.js
@@ -2,18 +2,18 @@ require('dotenv').config();
 const app = require('./app');
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.DATABASE, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+// mongoose.connect(process.env.DATABASE, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// });
 
-mongoose.connection
-  .on('open', () => {
-    console.log('Mongoose connection open');
-  })
-  .on('error', (err) => {
-    console.log(`Connection error: ${err.message}`);
-  });
+// mongoose.connection
+//   .on('open', () => {
+//     console.log('Mongoose connection open');
+//   })
+//   .on('error', (err) => {
+//     console.log(`Connection error: ${err.message}`);
+//   });
 
 const server = app.listen(3000, () => {
   console.log(`Express is running on port ${server.address().port}`);
