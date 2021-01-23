401336ddb79fbabeb258e0cceb36a9c586340874
Nick Norman <nicknorm007@gmail.com>
2021-01-23 11:26:45 -0500

    add cors

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require('express');
 const path = require('path');
 const routes = require('./routes/index');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+
 const app = express();
+app.use(cors())
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
