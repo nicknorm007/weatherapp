578af89b9a296f5ebe86dfa4c710f9153c0345e6
Nick Norman <nicknorm007@gmail.com>
2021-01-31 21:27:17 -0500

    jenkins test

--- test/weatherTest.js
+++ test/weatherTest.js
@@ -5,7 +5,7 @@ describe("Simple color test", function() {
   describe("test color temp return", function() {
     it("return the right color", function() {
       const color = colors.determineTemperatureColor(91);
-      expect(color).to.equal(colors.tempcolor_hsla_map.very_hot);
+      expect(color).to.equal(colors.tempcolor_hsla_map.cold);
     });
   });
 });
